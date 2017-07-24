cee1d211e11091f65264b22926a9132565dfa224
trisafotp <trisafotp@gmail.com>
2017-07-24 11:02:58 -0400

    injected  into input controller. still working on getting the inputs form to show up in the view.

--- inputController.js
+++ inputController.js
@@ -1,6 +1,6 @@
 var app = angular.module('madLib');
 
-app.controller('inputCtrl', function($scope, storyService){
+app.controller('inputCtrl', function($scope, storyService, $location){
   $scope.transferInputs = function(userInputs){
       storyService.insertInputs(userInputs);
       $location.path('/story');
